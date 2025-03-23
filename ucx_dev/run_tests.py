517c921e7659123145a4d05110c1b43133a0930b
Sihlfall <jaybee@sihlfall.ch>
2025-03-23 05:46:20 +0100

    Working test script (with obsolete code)

--- ucx_dev/run_tests.py
+++ ucx_dev/run_tests.py
@@ -2,24 +2,28 @@
 
 import paramiko
 import queue
+import shlex
 import subprocess
 import threading
+import time
 
 LOCAL_RUN_OK = 0
+LOCAL_PIPES = 1
 LOCAL_RUN_ERR = -999
 
 REMOTE_RUN_OK = 0
+REMOTE_PIPES = 1
 REMOTE_RUN_ERR = -999
 VERBOSE = True
+DEFAULT_SSH_PORT = 22
 
 def print_verbose(msg):
   if VERBOSE:
     print(msg)
 
-def execute_local_binary(binary_path, keystroke):
+def execute_local_binary2(binary_path):
   try:
-    # Start the binary using subprocess.Popen to have access to stdin, stdout, and stderr
-    print(f"Executing local binary: {binary_path}...")
+    print_verbose(f"Executing local binary: {binary_path}...")
     process = subprocess.Popen(
       binary_path,                  # The command or binary to run
       stdin=subprocess.PIPE,         # Allow sending input to the process via stdin
@@ -28,23 +32,96 @@ def execute_local_binary(binary_path, keystroke):
       text=True                      # Work with text (strings) rather than bytes
     )
 
-    # Send the keystroke to the binary via stdin
-    print(f"Sending keystroke: {keystroke}")
-    process.stdin.write(keystroke + '\n')
-    process.stdin.flush()
-
-    # Wait for the process to finish
-    print("Waiting for the command to finish...")
-    stdout_text, stderr_text = process.communicate()  # Read the output and error (if any)
-
-    # Capture the return code (exit status)
-    exit_status = process.returncode
-
-    return (LOCAL_RUN_OK, (exit_status, stdout_text, stderr_text))
+    def communicate(input=None):
+      stdout_text, stderr_text = process.communicate(input=input)
+      exit_status = process.returncode
+      return (exit_status, stdout_text, stderr_text)
+    
+    return (process, communicate)
 
   except Exception as e:
+    print_verbose(f"An error occurred: {e}")
+    return (None, None)
+
+
+def execute_remote_binary2(binary, host='127.0.0.1', port=DEFAULT_SSH_PORT, username=None):
+  # ChatGPT was helpful in creating the following
+  try:
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    print_verbose(f"Connecting to {host}...")
+    ssh.connect(host, port=port, username=username)
+    
+    cmd = shlex.join(binary)
+    print_verbose(f"Executing command: {cmd}...")
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+
+    class Process:
+      def __init__(self, stdin, stdout, stderr):
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+
+    process = Process(stdin, stdout, stderr)
+
+    def communicate(input=None):
+      if input is not None:
+        process.stdin.write(input)
+        process.stdin.flush()
+    
+      print_verbose("Waiting for the command to finish...")
+      exit_status = stdout.channel.recv_exit_status()
+      stdout_text = stdout.read().decode('utf-8')
+      stderr_text = stderr.read().decode('utf-8')
+
+      ssh.close()
+      print_verbose("SSH connection closed.")
+
+      return (exit_status, stdout_text, stderr_text)            
+    
+    return (process, communicate)
+      
+  except Exception as e:
     print(f"An error occurred: {e}")
+    return (None, None)
+
+
+def execute_local_binary(result_queue, binary):
+  try:
+    print_verbose(f"Executing local binary: {binary_path}...")
+    process = subprocess.Popen(
+      binary,                  # The command or binary to run
+      stdin=subprocess.PIPE,         # Allow sending input to the process via stdin
+      stdout=subprocess.PIPE,        # Capture the output
+      stderr=subprocess.PIPE,        # Capture the error
+      text=True                      # Work with text (strings) rather than bytes
+    )
+
+    result_queue.put( (LOCAL_PIPES, process) )
+
+    while process.poll() is None:
+      time.sleep(0.05)
+
+    print_verbose("Finished")
+
+  except Exception as e:
+    print_verbose(f"An error occurred: {e}")
     return (LOCAL_RUN_ERR, ())
+
+def local_communicate(process, input):
+  stdout_text, stderr_text = process.communicate(input=input)
+  exit_status = process.returncode
+  return (exit_status, stdout_text, stderr_text)
+
+def obsolete():
+  if keystroke is not None:
+    # Send the keystroke to the binary via stdin
+    print_verbose(f"Sending keystroke: {keystroke}")
+    process.stdin.write(keystroke + '\n')
+    process.stdin.flush()
+  print_verbose("Waiting for the command to finish...")
+
+  result_queue.put( (LOCAL_RUN_OK, (exit_status, stdout_text, stderr_text)) )
 
 def execute_remote_binary(result_queue, host, port, username, binary_path, keystroke):
   # ChatGPT was helpful in creating the following
@@ -56,10 +133,12 @@ def execute_remote_binary(result_queue, host, port, username, binary_path, keyst
     
     print_verbose(f"Executing binary: {binary_path}...")
     stdin, stdout, stderr = ssh.exec_command(binary_path)
-    
-    print_verbose(f"Sending keystroke: {keystroke}")
-    stdin.write(keystroke + '\n')
-    stdin.flush()
+    result_queue.put( (REMOTE_PIPES, (ssh, stdin, stdout, stderr)) )
+
+    if keystroke is not None:
+      print_verbose(f"Sending keystroke: {keystroke}")
+      stdin.write(keystroke + '\n')
+      stdin.flush()
     
     print_verbose("Waiting for the command to finish...")
     exit_status = stdout.channel.recv_exit_status()
@@ -79,11 +158,23 @@ def execute_remote_binary(result_queue, host, port, username, binary_path, keyst
     print(f"An error occurred: {e}")
     result_queue.put( (REMOTE_RUN_ERR, ()) )
 
-def start_in_thread(host, args):
+def remote_communicate(process, input=None):
+  (stdin, stdout, stderr) = process
+  if input is not None:
+    stdin.write(input)
+    stdin.flush()
+
+
+def start_in_thread(fn, args):
   result_queue = queue.Queue()
-  thread = threading.Thread(target=execute_remote_binary, args=(result_queue,) + args)
+  thread = threading.Thread(target=fn, args=(result_queue,) + args)
   thread.start()
   return (thread, result_queue)
+
+def run(fn, args):
+  result_queue = queue.Queue()
+  fn (result_queue, *args)
+  return result_queue
 
 host = '192.168.0.205'
 port = 22  # Default SSH port
@@ -91,20 +182,13 @@ username = 'jbecker'
 
 def test01_run():
   binary_path = '/home/cluster/tryout/test01'
-  keystroke = 'q'  # The keystroke you want to send to the binary
+  keystroke = 'q'
   print('Running test01:')
 
-  thread, result_queue = start_in_thread(
-    execute_remote_binary,
-    (host, port, username, binary_path, keystroke)
-  )
+  process, communicate = execute_remote_binary2(binary_path, host=host, port=port, username=username)
 
-  thread.join()
-
-  remote_run_status, remote_run_result = result_queue.get()
-
-  if remote_run_status == REMOTE_RUN_OK:
-    exit_status, stdout_text, stderr_text = remote_run_result
+  if process is not None:
+    exit_status, stdout_text, stderr_text = communicate(input='q\n')
     if exit_status == 0:
       print('Success')
     else:
@@ -117,13 +201,12 @@ def test01_run():
 
 def test01b_run():
   binary_path = './bin/test01'
-  keystroke = 'q'  # The keystroke you want to send to the binary
   print('Running test01b:')
 
-  local_run_status, local_run_result = execute_local_binary(binary_path, keystroke)
+  process, communicate = execute_local_binary2(binary_path)
 
-  if local_run_status == LOCAL_RUN_OK:
-    exit_status, stdout_text, stderr_text = local_run_result
+  if process is not None:
+    exit_status, stdout_text, stderr_text = communicate(input='q\n')
     if exit_status == 0:
       print('Success')
     else:
@@ -132,6 +215,69 @@ def test01b_run():
       print(f'Remote stdout:\n{stdout_text}')
       print(f'Remote stderr:\n{stderr_text}')
   else:
-    print("Remote run failed")
+    print("Local run failed")
 
-test01b_run()
+def test02_run():
+  local_binary_path = './bin/test02'
+  print('Running test02 (local/local):')
+
+  server_process, server_communicate = execute_local_binary2([ local_binary_path, 's', '9000' ])
+  client_process, client_communicate = execute_local_binary2([ local_binary_path, 'c', '127.0.0.1', '9000' ])
+
+  if server_process is not None and client_process is not None:
+    client_exit_status, client_stdout_text, client_stderr_text = client_communicate()
+    server_exit_status, server_stdout_text, server_stderr_text = server_communicate(input='q\n')
+
+    if server_exit_status == 0 and client_exit_status == 0:
+      print('Success')
+    else:
+      print('Test failed')
+      if server_exit_status != 0:
+        print(f'Server exit code: {server_exit_status}')
+        print(f'Server stdout:\n{server_stdout_text}')
+        print(f'Server stderr:\n{server_stderr_text}')
+      if client_exit_status != 0:
+        print(f'Client exit code: {client_exit_status}')
+        print(f'Client stdout:\n{client_stdout_text}')
+        print(f'Client stderr:\n{client_stderr_text}')
+  else:
+    if server_process is None:
+      print("Server run failed")
+    if client_process is None:
+      print("Client run failed")
+
+def test02b_run():
+  local_binary_path = './bin/test02'
+  remote_binary_path = '/home/cluster/tryout/test02'
+  print('Running test02 (remote/local):')
+
+  server_process, server_communicate = execute_remote_binary2(
+    [ remote_binary_path, 's', '9000' ],
+    host=host, username=username
+  )
+  client_process, client_communicate = execute_local_binary2([ local_binary_path, 'c', '192.168.0.205', '9000' ])
+
+  if server_process is not None and client_process is not None:
+    client_exit_status, client_stdout_text, client_stderr_text = client_communicate()
+    server_exit_status, server_stdout_text, server_stderr_text = server_communicate(input='q\n')
+
+    if server_exit_status == 0 and client_exit_status == 0:
+      print('Success')
+    else:
+      print('Test failed')
+      if server_exit_status != 0:
+        print(f'Server exit code: {server_exit_status}')
+        print(f'Server stdout:\n{server_stdout_text}')
+        print(f'Server stderr:\n{server_stderr_text}')
+      if client_exit_status != 0:
+        print(f'Client exit code: {client_exit_status}')
+        print(f'Client stdout:\n{client_stdout_text}')
+        print(f'Client stderr:\n{client_stderr_text}')
+  else:
+    if server_process is None:
+      print("Server run failed")
+    if client_process is None:
+      print("Client run failed")
+
+
+test02b_run()
---
+++ ucx_dev/src/test02.c
@@ -0,0 +1,103 @@
+#include "oob.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static int run_server(uint16_t host_port)
+{
+  char * s = "Hello Urs and Guenti ";
+  size_t sl = strlen(s);
+  int n = 30;
+
+  char * response_string = (char *) calloc (n * sl + 1, 1);
+  {
+    char * p = response_string;
+    int i = 0;
+    for (; i < n; ++i, p += sl) memcpy (p, s, sl);
+    * p = '\0';
+  }
+
+  struct ucx_dev_oob_response const response = {
+      .data = (unsigned char *)response_string,
+      .length = strlen (response_string)
+  };
+  printf ("Response length: %ld\n", response.length);
+
+  struct ucx_dev_oob_server_thread server_thread;
+
+  if (ucx_dev_oob_server_initialize (&server_thread, host_port, 3, &response))
+  {
+      printf ("Could not start server.\n");
+      return 1;
+  }
+
+  printf("Server is running in a separate thread. Press any key to stop.\n");
+
+  /* Wait for keypress */
+  getc (stdin);
+
+  printf ("Stopping server...\n");
+  ucx_dev_oob_server_destroy (&server_thread);
+  printf ("Server stopped.\n");
+
+  return 0;
+}
+
+int run_client (char const * peer_ip, uint16_t peer_port)
+{
+  struct ucx_dev_oob_response response;
+
+  if (ucx_dev_oob_client_make_request(peer_ip, peer_port, &response)) {
+      return 1;
+  }
+
+  char * text = calloc(response.length + 1, sizeof (char));
+  memcpy (text, response.data, response.length);
+  ucx_dev_oob_client_cleanup_response (&response);
+
+  printf("Text read: %s\n", text);
+
+  return 0;
+}
+
+static void abort_with_usage_message (void)
+{
+  fprintf(stderr, "Usage:\ntest02 s host_port\ntest02 c peer_ip peer_port\n");
+  exit (1);
+}
+
+int main (int argc, char ** argv)
+{
+  int ret = 0;
+
+  if (argc < 2) abort_with_usage_message ();
+
+  if (!strcmp (argv[1], "s"))
+  {
+    if (argc != 3) abort_with_usage_message ();
+
+    int tmp = atoi (argv[2]);
+    if (tmp > 0xffff || tmp < 0) abort_with_usage_message ();
+    uint16_t host_port = (uint16_t) tmp;
+
+    ret = run_server (host_port);
+  }
+  else if (!strcmp (argv[1], "c"))
+  {
+    if (argc != 4) abort_with_usage_message ();
+
+    char const * peer_ip = argv[2];
+
+    int tmp = atoi (argv[3]);
+    if (tmp > 0xffff || tmp < 0) return 1;
+    uint16_t peer_port = (uint16_t) tmp;
+
+    ret = run_client (peer_ip, peer_port);
+  }
+  else
+  {
+    return 1;
+  }
+
+  return ret;
+}
