abc6957db7e7de2c494fb5a524768d09883a9fb1
Sihlfall <jaybee@sihlfall.ch>
2025-03-24 10:48:24 +0100

    Intermediate (with stubs)

--- ucx_dev/src/test02.c
+++ ucx_dev/src/test02.c
@@ -1,11 +1,117 @@
 #include "oob.h"
+#include "GPI2_UCX.h"
 #include "ucp/api/ucp.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-static int run_server(uint16_t host_port)
+static
+ucs_status_t
+initialize_ucp_context (ucp_context_h * ucp_context)
+{
+  ucs_status_t status = ucp_init (
+    & (ucp_params_t) {
+      .field_mask = UCP_PARAM_FIELD_FEATURES,
+      .features = UCP_FEATURE_TAG | UCP_FEATURE_STREAM
+    },
+    NULL, ucp_context
+  );
+  return status;
+}
+
+static
+void
+cleanup_ucp_context (ucp_context_h ucp_context)
+{
+  ucp_cleanup (ucp_context);
+}
+
+static
+ucs_status_t
+create_oob_server_worker (ucp_context_h ucp_context, ucp_worker_h * oob_server_worker)
+{
+  ucs_status_t status = ucp_worker_create (
+    ucp_context,
+    & (ucp_worker_params_t) {
+      .field_mask = UCP_WORKER_PARAM_FIELD_THREAD_MODE,
+      .thread_mode = UCS_THREAD_MODE_SINGLE
+    },
+    oob_server_worker
+  );
+  return status;
+}
+
+static
+void
+destroy_oob_server_worker (ucp_worker_h oob_server_worker)
+{
+  ucp_worker_destroy (oob_server_worker);
+}
+
+static
+ucs_status_t
+create_ucx_ctx_default_worker (gaspi_ucx_ctx * ucx_ctx)
+{
+  ucp_worker_h worker;
+  ucs_status_t status = ucp_worker_create (
+    ucx_ctx->wpool->ucp_ctx,
+    & (ucp_worker_params_t) {
+      .field_mask = UCP_WORKER_PARAM_FIELD_THREAD_MODE,
+      .thread_mode = UCS_THREAD_MODE_SINGLE
+    },
+    &worker
+  );
+  if (status != UCS_OK) return status;
+
+  ucx_ctx->wpool->default_worker = worker;
+  return UCS_OK;
+}
+
+static
+void
+destroy_ucx_ctx_default_worker (gaspi_ucx_ctx * ucx_ctx)
+{
+  ucp_worker_destroy (ucx_ctx->wpool->default_worker);
+}
+
+static
+int
+initialize_stub_ucx_ctx (gaspi_ucx_ctx * ucx_ctx)
+{
+  * ucx_ctx = (gaspi_ucx_ctx) {
+    .wpool = (ucx_wpool_t *) calloc (1, sizeof (ucx_wpool_t))
+  };
+
+  {
+    ucp_context_h ucp_context;
+    if (initialize_ucp_context(&ucp_context) != UCS_OK)
+    {
+      fprintf (stderr, "Initializing UCP context failed\n");
+      return 1;
+    }
+    ucx_ctx->wpool->ucp_ctx = ucp_context;
+  }
+
+  if (create_ucx_ctx_default_worker (ucx_ctx) != UCS_OK) goto err_create_default_worker;
+  return 0;
+
+err_create_default_worker:
+  cleanup_ucp_context (ucx_ctx->wpool->ucp_ctx);
+  return 1;
+}
+
+static
+void
+cleanup_stub_ucx_ctx (gaspi_ucx_ctx * ucx_ctx)
+{
+  destroy_ucx_ctx_default_worker (ucx_ctx);
+  cleanup_ucp_context (ucx_ctx->wpool->ucp_ctx);
+}
+
+static
+int
+run_server (ucp_context_h ucp_context, uint16_t host_port)
 {
   char * s = "Hello Urs and Guenti ";
   size_t sl = strlen(s);
@@ -25,39 +131,12 @@ static int run_server(uint16_t host_port)
   };
   printf ("Response length: %ld\n", response.length);
 
-  /* initialize UCP context */
-  ucp_context_h ucp_context;
-  {
-    ucs_status_t status = ucp_init (
-      & (ucp_params_t) {
-        .field_mask = UCP_PARAM_FIELD_FEATURES,
-        .features = UCP_FEATURE_TAG | UCP_FEATURE_STREAM
-      },
-      NULL, &ucp_context
-    );
-    if (status != UCS_OK)
-    {
-        fprintf (stderr, "Initializing UCP context failed\n");
-        exit (1);
-    }
-  }
-
   /* create worker */
   ucp_worker_h ucp_worker;
+  if (create_oob_server_worker (ucp_context, &ucp_worker) != UCS_OK)
   {
-    ucs_status_t status = ucp_worker_create (
-      ucp_context,
-      & (ucp_worker_params_t) {
-        .field_mask = UCP_WORKER_PARAM_FIELD_THREAD_MODE,
-        .thread_mode = UCS_THREAD_MODE_SINGLE
-      },
-      &ucp_worker
-    );
-    if (status != UCS_OK)
-    {
-      fprintf (stderr, "Creating UCP worker failed\n");
-      exit (1);
-    }
+    fprintf (stderr, "Creating UCP worker failed\n");
+    return 1;
   }
 
   struct ucx_dev_oob_server_thread server_thread;
@@ -77,55 +156,22 @@ static int run_server(uint16_t host_port)
   ucx_dev_oob_server_destroy (&server_thread);
   printf ("Server stopped.\n");
 
-  ucp_worker_destroy (ucp_worker);
-  ucp_cleanup (ucp_context);
+  destroy_oob_server_worker (ucp_worker);
 
   return 0;
 }
 
-int run_client (char const * peer_ip, uint16_t peer_port)
+static
+int
+run_client (gaspi_ucx_ctx * ucx_ctx, char const * peer_ip, uint16_t peer_port)
 {
-  /* initialize UCP context */
-  ucp_context_h ucp_context;
-  {
-    ucs_status_t status = ucp_init (
-      & (ucp_params_t) {
-        .field_mask = UCP_PARAM_FIELD_FEATURES,
-        .features = UCP_FEATURE_TAG | UCP_FEATURE_STREAM
-      },
-      NULL, &ucp_context
-    );
-    if (status != UCS_OK)
-    {
-        fprintf (stderr, "Initializing UCP context failed\n");
-        exit (1);
-    }
-  }
-
-  /* create worker */
-  ucp_worker_h ucp_worker;
-  {
-    ucs_status_t status = ucp_worker_create (
-      ucp_context,
-      & (ucp_worker_params_t) {
-        .field_mask = UCP_WORKER_PARAM_FIELD_THREAD_MODE,
-        .thread_mode = UCS_THREAD_MODE_SINGLE
-      },
-      &ucp_worker
-    );
-    if (status != UCS_OK)
-    {
-      fprintf (stderr, "Creating UCP worker failed\n");
-      exit (1);
-    }
-  }
-
   struct ucx_dev_oob_response response;
 
-  if (ucx_dev_oob_client_make_request(ucp_worker, peer_ip, peer_port, &response)) {
+  if (ucx_dev_oob_client_make_request(ucx_ctx->wpool->default_worker, peer_ip, peer_port, &response))
+  {
       return 1;
   }
-
+  
   char * text = calloc(response.length + 1, sizeof (char));
   memcpy (text, response.data, response.length);
   ucx_dev_oob_client_cleanup_response (&response);
@@ -135,44 +181,80 @@ int run_client (char const * peer_ip, uint16_t peer_port)
   return 0;
 }
 
-static void abort_with_usage_message (void)
+static
+void
+abort_with_usage_message (void)
 {
   fprintf(stderr, "Usage:\ntest02 s host_port\ntest02 c peer_ip peer_port\n");
   exit (1);
 }
 
-int main (int argc, char ** argv)
+struct config {
+  int is_server;
+  union {
+    struct {
+      uint16_t host_port;
+    } server;
+    struct {
+      char const * peer_ip;
+      uint16_t peer_port;
+    } client;
+  } v;
+};
+
+int
+main (int argc, char ** argv)
 {
+  struct config config = {0};
+
   int ret = 0;
 
   if (argc < 2) abort_with_usage_message ();
 
   if (!strcmp (argv[1], "s"))
   {
+    config.is_server = 1;
+
     if (argc != 3) abort_with_usage_message ();
 
     int tmp = atoi (argv[2]);
     if (tmp > 0xffff || tmp < 0) abort_with_usage_message ();
-    uint16_t host_port = (uint16_t) tmp;
-
-    ret = run_server (host_port);
+    config.v.server.host_port = (uint16_t) tmp;
   }
   else if (!strcmp (argv[1], "c"))
   {
+    config.is_server = 0;
+
     if (argc != 4) abort_with_usage_message ();
 
-    char const * peer_ip = argv[2];
+    config.v.client.peer_ip = argv[2];
 
     int tmp = atoi (argv[3]);
-    if (tmp > 0xffff || tmp < 0) return 1;
-    uint16_t peer_port = (uint16_t) tmp;
-
-    ret = run_client (peer_ip, peer_port);
+    if (tmp > 0xffff || tmp < 0) abort_with_usage_message ();
+    config.v.client.peer_port = (uint16_t) tmp;
   }
   else
   {
-    return 1;
+    abort_with_usage_message ();
   }
+
+  gaspi_ucx_ctx ucx_ctx;
+  if (initialize_stub_ucx_ctx (&ucx_ctx))
+  {
+    fprintf (stderr, "Initializing GASPI UCX context failed\n");
+    exit (1);
+  }
+
+  if (config.is_server)
+  {
+    ret = run_server (ucx_ctx.wpool->ucp_ctx, config.v.server.host_port);
+  }
+  else
+  {
+    ret = run_client (&ucx_ctx, config.v.client.peer_ip, config.v.client.peer_port);
+  }
+
+  cleanup_stub_ucx_ctx (&ucx_ctx);
 
   return ret;
 }
