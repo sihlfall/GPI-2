b89686537dc6b9c02bf345307ff17d8cc94f6020
Sihlfall <jaybee@sihlfall.ch>
2025-05-09 06:41:54 +0200

    Separated capacity from buffer

--- ucx_dev/src/plqueue_timing_cpp_interop.c
+++ ucx_dev/src/plqueue_timing_cpp_interop.c
@@ -4,7 +4,8 @@
 #include <stdalign.h>
 
 struct plqueue_u64_rw {
-  struct plqueue_u64 q;
+  int log2_capacity;
+  struct plqueue_u64_entry * entries;
   alignas(64) m_cursor_t write_cursor;
   alignas(64) m_cursor_t read_cursor;
 };
@@ -13,24 +14,26 @@ struct plqueue_u64_rw * queue_create (void) {
   size_t log2_capacity = 6;
   struct plqueue_u64_rw * q = calloc (1, sizeof(*q));
   *q = (struct plqueue_u64_rw) {
-    .q = {
-      .log2_capacity = log2_capacity,
-      .entries = calloc (1 << log2_capacity, sizeof(*q->q.entries))
-    }
+    .log2_capacity = log2_capacity,
+    .entries = calloc (1 << log2_capacity, sizeof(*q->entries))
   };
   return q;
 }
 
 void queue_destroy (struct plqueue_u64_rw * q) {
-  if (q) { free (q->q.entries); free (q); }
+  if (q) { free (q->entries); free (q); }
 }
 
 int queue_enqueue (struct plqueue_u64_rw * q, uint64_t payload) {
-  return plqueue_mp_enqueue_u64 (&q->q, &q->write_cursor, payload);
+  return plqueue_mp_enqueue_u64 (
+    q->log2_capacity, q->entries, &q->write_cursor, payload
+  );
 }
 
 int queue_dequeue (struct plqueue_u64_rw * q, uint64_t * payload) {
-  struct maybe_payload_u64 r = plqueue_mc_dequeue_u64 (&q->q, &q->read_cursor);
+  struct maybe_payload_u64 r = plqueue_mc_dequeue_u64 (
+    q->log2_capacity, q->entries, &q->read_cursor
+  );
   if (r.has_value) {
     *payload = r.payload;
     return 1;
@@ -40,5 +43,7 @@ int queue_dequeue (struct plqueue_u64_rw * q, uint64_t * payload) {
 }
 
 int queue_is_empty (struct plqueue_u64_rw * q) {
-  return plqueue_mc_is_empty_u64 (&q->q, &q->read_cursor);
+  return plqueue_mc_is_empty_u64 (
+    q->log2_capacity, q->entries, &q->read_cursor
+  );
 }
